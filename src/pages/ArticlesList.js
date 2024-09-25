f2f33a65dd4967a4b25ab125506d6aef25542064
SMFaysalHaque <smfaysalhaque007@gmail.com>
2024-09-25 17:44:22 +0600

    first commit

---
+++ src/pages/ArticlesList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArticlesList = () => {
+  return (
+    <h1 className="sm:text-4xl text-2xl font-bold text-gray-900 my-6">
+        Articles
+    </h1>
+  )
+}
+
+export default ArticlesList
---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1 className="sm:text-4xl text-2xl font-bold text-gray-900 my-6">
+            Hello, Welcome to my Blog.
+        </h1>
+        <p className="mx-auto leading-relaxed text-base mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius praesentium sapiente ducimus quia hic repudiandae a asperiores, dolores exercitationem animi voluptatum veniam illo ut, officiis illum corrupti incidunt rerum consectetur eum perspiciatis maiores deleniti itaque quae mollitia? Necessitatibus, eius tempora aut quasi, deserunt ullam modi consequuntur esse voluptates ex facilis!
+        </p>
+        <p className="mx-auto leading-relaxed text-base mb-4">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam illum temporibus magnam quod perferendis possimus nisi perspiciatis expedita exercitationem error nemo at amet nulla, explicabo fuga quidem ipsam est labore?
+        </p>
+        <p className="mx-auto leading-relaxed text-base mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab impedit, similique iure quae soluta, ratione temporibus minus reiciendis architecto laudantium assumenda voluptate dolorem labore! Quo quidem cum voluptatem non! Repellendus quaerat nam dolores eius ab debitis voluptate possimus aliquam id.
+        </p>
+    </div>
+  )
+}
+
+export default Home
