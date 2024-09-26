f98da820bf7a47c8249dd6c6e52d001c939f1db3
SMFaysalHaque <smfaysalhaque007@gmail.com>
2024-09-27 00:08:45 +0600

    feat: add 404 page and favicon

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import articleContent from './article-content'
+import NotFound from './NotFound'
 
 const Article = () => {
     const { name } = useParams()
     const article = articleContent.find((article) => article.name === name)
-    if (!article) return <h1>Article dose not exists!!!</h1>
+    if (!article) return <NotFound />
     return (
         <div>
             <h1 className="sm:text-4xl text-2xl font-bold text-gray-900 my-6">
---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1 className="sm:text-4xl text-2xl font-bold text-gray-900 my-6">
+                404: Page not found.
+            </h1>
+        </div>
+    )
+}
+
+export default NotFound
